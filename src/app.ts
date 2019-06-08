4e5b9374ad36537795c0b0f877941ba921e23ed8
Shota Kawashima <bysk.333@gmail.com>
2019-06-08 15:41:16 +0900

    Expressの初期化 (#1)
    * とりあえず起動
    
    * comp deploy

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log('hello world')
+import * as express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello Express'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}`))
