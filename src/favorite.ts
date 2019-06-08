44bad9de731f50358d859983b6d9913ae1e968d3
sKawashima <bysk.333@gmail.com>
2019-06-08 17:00:12 +0900

    ローカル→MongoDBへのPushを実装

--- src/favorite.ts
+++ src/favorite.ts
@@ -1,8 +1,27 @@
 import * as express from 'express'
 export const favoriteRouter = express.Router()
 
-import {FavoriteModel} from './model/favoriteModel'
-
 favoriteRouter.get('/', (_req, res) => {
   res.send('This is favorite API.')
+})
+
+import { FavoriteModel } from './model/favoriteModel'
+favoriteRouter.post('/', (req,res) => {
+  const Favorite:any = new FavoriteModel()
+
+  Favorite.user = req.query.user
+  Favorite.longitude = req.query.longitude
+  Favorite.latitude = req.query.latitude
+  Favorite.date = new Date()
+
+  Favorite.save((err: string) => {
+    if (err) {
+      console.error(err)
+      res.send(err)
+    } else {
+      res.json({
+        message: 'success'
+      })
+    }
+  })
 })
