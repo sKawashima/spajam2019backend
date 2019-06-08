dcdf88760de35a7b93c17d725f8bbcb9d9d37c40
sKawashima <bysk.333@gmail.com>
2019-06-08 16:12:20 +0900

    ひとまずコネクト

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-import * as express from 'express'
+import * as mongoose from 'mongoose'
 
+// mongoose.Promise = global.Promise
+mongoose.connect('mongodb+srv://mongo:2vpFSevtWpbPqF5F@cluster0-vbb6l.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connection.on('error', (err: string) => {
+  console.error('MongoDB connection error: ' + err)
+  process.exit(-1);
+})
+
+import * as express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello Express'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}`))
+app.listen(port, () => console.log(`Server Up.`))
